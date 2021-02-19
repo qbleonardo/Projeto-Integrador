f5f8b0a5aa73f5cee0d0b6f1ac6a27856929ae8a
Leonardo <qbleonardo@gmail.com>
2021-02-19 17:09:43 -0300

    Spring do Projeto Integrador

---
+++ educAcao/src/main/java/org/generation/educAcao/EducAcaoApplication.java
@@ -0,0 +1,13 @@
+package org.generation.educAcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducAcaoApplication.class, args);
+	}
+
+}
